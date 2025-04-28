24296dce70bdf0bb42a8ec788057b66330fe783b
Ayu322 <ad995722@gmail.com>
2025-04-28 06:46:26 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14564
+// customerId = 2
+let accountEmail = "ayush@gmail.com"
+var accountPassword = "1234"
+accountCity = "Delhi"
+
+accountEmail = "hd@fcgmail.com"
+accountPassword = "02689"
+accountCity = "jaipur"
+
+console.table([accountEmail, accountPassword, accountCity])
+
+// console.log(customerId) ctrl + slash to comment;
+
+/*
+  another way to comment....
+*/
+
+
