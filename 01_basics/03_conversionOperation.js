9d2f7af384adc813309707fcda7872095d6dbc86
Ayu322 <ad995722@gmail.com>
2025-04-28 08:22:09 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = 33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// number converted into number esiley "33" => 33
+//  "33abc" => NaN (not an number)
+// true => 1; false => 0;
+
+let isloggedIn = 1
+let BooleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(BooleanIsLoggedIn);
+
+// "1" => true; "0" => false
+
+// "" => false
+// "ayush" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
+
