a0553aaadfa1bfcdb1bffeb61ee0539409c30312
Ayu322 <ad995722@gmail.com>
2025-05-12 08:12:05 +0000

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,29 @@
+const myArr = [1, 8, 7, 6, 10, 89, 87]
+//  console.log(myArr[2]);
+//  myArr.push(5, 10, 98, 25, 30)
+//  myArr.pop()
+//  console.log(myArr);
+
+//  console.log(myArr.includes(15));
+//  console.log(myArr.indexOf(4));
+ const newArr = myArr.join()
+//  console.log(myArr);
+//  console.log(newArr);
+ 
+
+//slice , splice
+
+console.log("A", myArr);
+const myn1 = myArr.slice(1,3)
+console.log("B",myn1);
+
+const myn2 = myArr.splice(1,3)
+console.log("c", myn2);
+console.log(myn2);
+
+
+
+
+ 
+
+ 
