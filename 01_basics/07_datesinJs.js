146713d84a96ef6cec5f6fde02c8cc72c51b65e6
Ayu322 <ad995722@gmail.com>
2025-05-12 06:42:03 +0000

    date and time

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,25 @@
+//let myDate =  new Date();
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJSON());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toLocaleString());
+//console.log(myDate.toLocaleTimeString());
+//console.log(myDate.toString());
+
+let myCreateDate = new Date(2025 , 8 , 25);
+//console.log(myCreateDate.toDateString());
+
+let myTimeStamp =  Date.now()
+//console.log(myTimeStamp);
+//console.log(myCreateDate.getTime());
+
+//console.log(Math.floor((Date.now()/1000)));
+
+let newDate = new Date()
+ console.log(newDate.getFullYear());
+ console.log(newDate.getMonth());
+ console.log(newDate.getTime());
+ 
+ 
+ 
