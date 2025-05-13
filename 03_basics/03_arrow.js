e54b281fb8bc223c8f0d0e0fd3d74c1ab1b69263
Ayu322 <ad995722@gmail.com>
2025-05-13 08:20:57 +0000

    this and arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,23 @@
+const user = {
+    username: "ayush",
+    website: function () {
+        // console.log(this.username);
+        
+    }
+}
+
+user.website()
+
+
+
+
+// const add = (num1 , num2) => {
+//     return num1 + num2
+// }
+
+const add = (num1 , num2) => {
+    return num1 + num2
+}
+
+console.log(add(5 , 8));
+
